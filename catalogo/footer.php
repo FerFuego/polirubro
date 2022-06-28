00d3caa4e44acf97357d67de2911103470b283d0
Fer Catalano <fernando@hazak.tech>
2022-06-28 19:40:45 -0300

    Delete old elements

--- catalogo/footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-	<div id="footer">
-        <strong>Polirrubros Garro</strong> | Pasaje Bujados 173, 2550 Bell Ville, Córdoba | Tel.: (03537) 410102 | <a href="contacto.php">CONTACTO</a> | Desarrollo del Sitio: <a href="http://www.lucianocolmano.com.ar/" target="_blank">Luciano Colmano</a>
-    </div>
-</footer>
-<script src="../js/jquery.js"></script>
-<script src="../js/bootstrap.min.js"></script>
-<script src="../js/lightbox.js"></script>
--- catalogo/header.php
+++
@@ -1,5 +0,0 @@
-<header>
-    <div align="center">
-        <img src="../images/cabe_900.jpg" class="img-responsive">
-    </div>
-</header>
