0b6f4f19e4e24ea094703e07cbcfda87098094b3
Fer Catalano <fernando@hazak.tech>
2024-04-13 16:18:31 -0300

    Add - new banner

---
+++ inc/parts/banner-shopping.php
@@ -0,0 +1,26 @@
+<div class="banner-shopping">
+    <div class="container">
+        <div class="row">
+            <div class="banner-shopping__item col-3">
+                <i class="fa fa-shopping-cart fa-4x"></i>
+                <h4 class="banner-shopping__title">REALIZÁ TU PEDIDO</h4>
+                <p class="banner-shopping__text">El monto minimo de compra es de $50.000. Agrega al carrito los productos que quieras y completa los datos personales.</p>
+            </div>
+            <div class="banner-shopping__item col-3">
+                <i class="fa fa-whatsapp fa-4x"></i>
+                <h4 class="banner-shopping__title">ARMAMOS TU PEDIDO</h4>
+                <p class="banner-shopping__text">Nos pondremos en contacto con vos dentro de las 24hs para confirmar stock.</p>
+            </div>
+            <div class="banner-shopping__item col-3">
+                <i class="fa fa-credit-card fa-4x"></i>
+                <h4 class="banner-shopping__title">REALIZÁ EL PAGO</h4>
+                <p class="banner-shopping__text">Podes abonar por depósito bancario, transferencia o en efectivo en nuestro local.</p>
+            </div>
+            <div class="banner-shopping__item col-3">
+                <i class="fa fa-truck fa-4x"></i>
+                <h4 class="banner-shopping__title">ENTREGA DE TU PEDIDO</h4>
+                <p class="banner-shopping__text">Retira tu pedido en nuestro local o te lo enviamos por encomienda.</p>
+            </div>
+        </div>
+    </div>
+</div>
