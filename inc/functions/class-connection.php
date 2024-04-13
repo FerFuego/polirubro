2aa4d81096f5eab5728e20d2543ea2e9569381c7
Fer Catalano <fernando@hazak.tech>
2024-04-12 22:35:05 -0300

    fix connection

--- inc/functions/class-connection.php
+++ inc/functions/class-connection.php
@@ -9,17 +9,10 @@ class Connection {
     
 	function __construct() {
 
-		if (str_replace('"', '', getenv('ENVIRONMENT')) == 'production') {
-			$con['server'] = str_replace('"', '', getenv('SERVER'));
-			$con['base'] = str_replace('"', '', getenv('BASE'));
-			$con['user'] = str_replace('"', '', getenv('USER'));
-			$con['pass'] = str_replace('"', '', getenv('PASS'));
-		} else {
-			$con['server'] = str_replace('"', '', getenv('LOCAL_SERVER'));
-			$con['base'] = str_replace('"', '', getenv('LOCAL_BASE'));
-			$con['user'] = str_replace('"', '', getenv('LOCAL_USER'));
-			$con['pass'] = str_replace('"', '', getenv('LOCAL_PASS'));
-		}
+		$con['server'] = str_replace('"', '', getenv('SERVER'));
+		$con['base'] = str_replace('"', '', getenv('BASE'));
+		$con['user'] = str_replace('"', '', getenv('USER'));
+		$con['pass'] = str_replace('"', '', getenv('PASS'));
 
 		$result = new mysqli($con['server'], $con['user'], $con['pass'], $con['base']);
         
