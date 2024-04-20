1dd4898491c37e473ee24429d835e37e8cb370b8
Fer Catalano <fernando@hazak.tech>
2024-04-20 19:25:52 -0300

    Add - dynamic map section

--- inc/parts/map-section.php
+++ inc/parts/map-section.php
@@ -1,13 +1,13 @@
 <div class="map">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1680.0084267729828!2d-62.682554186075706!3d-32.63237464547776!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95cbfafedd28fcbd%3A0x22a2c4a3b026f7d9!2sSgto%20Cabral%20234%2C%20Bell%20Ville%2C%20C%C3%B3rdoba!5e0!3m2!1ses!2sar!4v1611518858719!5m2!1ses!2sar" height="500" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-    <div class="map-inside">
+    <iframe width="100%" height="500" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?q=<?php echo str_replace([' ',','],['%20','%2C'],$general->direccion); ?>(Sterra%20Distribuidora)&amp;output=embed"></iframe>
+    <!-- <div class="map-inside">
         <i class="icon_pin"></i>
         <div class="inside-widget">
             <h4>Bell Ville</h4>
             <ul>
-                <li>Telefono: <?php echo $general->telefono; ?></li>
-                <li>Direccion: <?php echo $general->direccion; ?></li>
+                <li>Telefono: <?php //echo $general->telefono; ?></li>
+                <li>Direccion: <?php //echo $general->direccion; ?></li>
             </ul>
         </div>
-    </div>
+    </div> -->
 </div>
