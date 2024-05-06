65ce10c5af5f6dc80b84e5ec79887956d48019cd
Fer Catalano <fernando@hazak.tech>
2024-05-06 19:54:45 -0300

    rollback captcha

--- inc/functions/ajax-requests.php
+++ inc/functions/ajax-requests.php
@@ -16,13 +16,13 @@ if( !empty($_POST) && isset($_POST['action']) && $_POST['action'] == 'actionLogi
     $login = 'false';
     $updated = false;
 
-    // $request = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".Polirubro::get_google_api()."&response=".$recaptcha."&remoteip=".$_SERVER['REMOTE_ADDR']);
-    // $response = json_decode($request);
+    $request = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".Polirubro::get_google_api()."&response=".$recaptcha."&remoteip=".$_SERVER['REMOTE_ADDR']);
+    $response = json_decode($request);
     
-    // if ( $response->success === false ) {
-    //     echo 'Captcha Incorrecto!';
-    //     die();
-    // }
+    if ( $response->success === false ) {
+        echo 'Captcha Incorrecto!';
+        die();
+    }
 
     $access = new Login($user, $pass);
     $result = $access->loginProcess();
