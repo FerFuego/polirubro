745c406a7f0da783bb5271b7017623e35fc4b0e6
Luciano Colmano <lcolmano@gmail.com>
2024-06-11 19:19:41 -0300

    arregla img-svg

--- inc/parts/cart-empty.php
+++ inc/parts/cart-empty.php
@@ -2,7 +2,7 @@
     <div class="row">
         <div class="col-lg-12">
             <h2 class="text-danger text-center">No tienes ningún pedido abierto</h2>
-            <p class="text-center"><img src="../img/cart-empy.svg"></p>
+            <p class="text-center"><img src="../../img/cart/cart-empty.svg"></p>
         </div>
     </div>
 </div>
