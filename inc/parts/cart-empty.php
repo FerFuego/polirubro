d60504afc4ba662755c95fd3882f75de7bb181bb
Luciano Colmano <lcolmano@gmail.com>
2024-07-01 16:06:11 -0300

    achico logo empty car

--- inc/parts/cart-empty.php
+++ inc/parts/cart-empty.php
@@ -2,7 +2,7 @@
     <div class="row">
         <div class="col-lg-12">
             <h2 class="text-danger text-center">No tienes ningún pedido abierto</h2>
-            <p class="text-center"><img src="../../img/cart/cart-empty.svg"></p>
+            <p class="text-center"><img src="../../img/cart/cart-empty.svg" width="128"></p>
         </div>
     </div>
 </div>
