c084dcfae77b98a75071efe81bb2f93566be982c
Fer Catalano <fernando@hazak.tech>
2025-12-13 18:53:59 -0300

    Add - change title

--- inc/parts/featured-products-section.php
+++ inc/parts/featured-products-section.php
@@ -9,7 +9,7 @@
             <div class="row">
                 <div class="col-lg-12">
                     <div class="section-title">
-                        <h2>Lista de Últimos Productos</h2>
+                        <h2>Nuevos Productos</h2>
                     </div>
                 </div>
             </div>
